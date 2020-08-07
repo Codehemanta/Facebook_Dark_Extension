8525911b33eed07e928301aa90766f6fdceb30d2
Hemanta_Gayen <69314427+Codehemanta@users.noreply.github.com>
2020-08-07 20:18:43 +0530

    Add files via upload

---
+++ call.js
@@ -0,0 +1,16 @@
+let rXasa ='vhasrxA';let saj = 'Developer';
+
+let eaws ='vhasrxA';
+document.getElementsByClassName(rXasa)[0].innerHTML = "Developer";
+
+
+document.getElementsByClassName(eaws)[0].onclick = function () {
+	window.open('https://www.facebook.com/photo.php?fbid=1110445909344443&set=a.100112610377783&type=3&theater');};
+
+	
+	
+	
+
+
+
+
---
+++ content.js
@@ -0,0 +1,15 @@
+console.log("start faceboook");
+let asaa = document.getElementsByClassName('_4962 ');
+let a3as = document.getElementsByClassName('_4-u2 ');
+for (elt of a3as) {
+	let valx20 = 'solid 1.9px #000000';      
+	elt.style['border-left'] = valx20;
+	elt.style['border-top'] = valx20;
+	elt.style['border-bottom'] = valx20;
+}
+for (elt of asaa) {
+	elt.style['background-color'] = '#c7c7c7';
+	elt.style['border-radius'] = '60px';
+	elt.style['margin'] = '4px';  
+}
+
